bbd41c0deddb463a2848e03ca88038c86920bd51
Rohit kumar pandey <80757398+pandeyrohit1234@users.noreply.github.com>
2021-05-16 04:11:32 -0700

    Delete ProductCatalogStreams directory

--- ProductCatalogStreams/Category_Library/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Category_Library
-{
-    public class Class1
-    {
-    }
-}
--- ProductCatalogStreams/Product_Library/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Product_Library
-{
-    public class Class1
-    {
-    }
-}
